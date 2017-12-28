44e40bc11566aa548a1f0e48b7a71bc22f281212
Mikami, Takeshi <takeshi.mikami@gmail.com>
2017-12-28 11:32:27 +0900

    add travisci

---
+++ test/erb-to-ast-test.js
@@ -0,0 +1,8 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const assert = require("power-assert");
+const test = require("textlint-ast-test").test;
+
+// no testcase
